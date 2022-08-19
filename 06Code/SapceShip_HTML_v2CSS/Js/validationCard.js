67c11dad0b3b314a19b5aa632058273808d7a595
Santiago Aldair Guanoluisa Ruiz <saguanoluisa@espe.edu.ec>
2022-08-18 20:59:37 -0500

    Validation Card 2.0

--- 06Code/SapceShip_HTML_v2CSS/Js/validationCard.js
+++ 06Code/SapceShip_HTML_v2CSS/Js/validationCard.js
@@ -1,41 +1,47 @@
-function validateCard(){
-    var expRegPhone = /^.{16}$/;
+const expressions = {
+    onlyLetters: /^[a-zA-Z\s_\d]{1,30}$/,
+    Card: /^.{16}$/,
+    CCV: /^.{3}$/,
+}
+
+function validateCard() {
+    var expRegCard = /^[0123456789\s]{16}$/;
     var UserCard = document.getElementById("txtCard");
-    if (!expRegCard.exec(UserCard.value)){
-        alert("El numero de su tarjeta debe tener 16 digitos.");
+    if (!expRegCard.exec(UserCard.value)) {
+        alert("El numero de su tarjeta debe tener 16 digitos y sin letras.");
 
         return false;
 
-    } else{
-    
+    } else {
+
         return true;
     }
 }
 
-function validateCCV(){
-    var expRegPhone = /^.{12}$/;
-    var UserCCV = document.getElementById("txtCCV");
-    if (!expRegCCV.exec(UserCCV.value)){
-        alert("El numero de CCV debe tener 3 digitos.");
-
-        return false;
-
-    } else{
-    
-        return true;
-    }
-}
-
-function validateFullName(){
-    var expRegFullName = /^[a-zA-Z\s_\d]{1,50}$/;
+function validateFullName() {
+    var expRegFullName = /^[a-zA-ZÑñÁáÉéÍíÓóÚúÜü\s]{1,50}$/;
     var UserFullName = document.getElementById("txtNombre");
-    if (!expRegFullName.exec(UserFullName.value)){
+    if (!expRegFullName.exec(UserFullName.value)) {
         alert("El nombre solo debe tener caracteres.");
 
         return false;
 
-    } else{
-    
+    } else {
+
+        return true;
+    }
+}
+
+function validateCCV() {
+    var expRegCCV = /^[0123456789\s]{3}$/;
+    var UserCCV = document.getElementById("txtCCV");
+    if (!expRegCCV.exec(UserCCV.value)) {
+        alert("El numero de CCV debe tener 3 digitos y sin letras.");
+
+        return false;
+
+    } else {
+
         return true;
     }
 }
